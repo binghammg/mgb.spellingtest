97550bb7924d8147cacfc8f1bda4d88a6900e4f9
Mike Bingham <binghammg@gmail.com>
2013-07-27 21:31:31 -0600

    Initial Service development.

---
+++ mgb.spellingtest/SpellingTestService.svc.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace mgb.spellingtest
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "SpellingTestService" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select SpellingTestService.svc or SpellingTestService.svc.cs at the Solution Explorer and start debugging.
+    public class SpellingTestService : ISpellingTestService
+    {
+        public void DoWork()
+        {
+        }
+    }
+}
